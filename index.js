df15e99985cc988c025c2345dffcd57ef66a7ea7
vimarshmishra37 <vimarshmishra37@gmail.com>
2024-09-15 18:33:38 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require("express"); 
+const mongoose=require("mongoose");
 const port = 3000;
 const app=express();
+mongoose.connect("mongodb+srv://vim:XXBqgiMHu3B59kNx@ssi.1z8xe.mongodb.net/?retryWrites=true&w=majority&appName=SSI",
+    {
+   
+    }).then((x)=>{console.log("connected to mongodb")}).catch((e)=>{console.log(e)});
 app.get("/",(req,res)=>{
     res.send("hello");
 })
---
+++ models/users.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+const user = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String,
+    age: Number,
+    gender: String,
+    dob: Date,
+    address: String,
+    phone: Number,
+    role: String
+});
+
+module.exports=mongoose.model("users",user)
